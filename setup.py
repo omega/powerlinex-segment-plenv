4568533537629d63f959b115be5d15e9e2ac0c3b
Andreas Marienborg <andreas.marienborg@gmail.com>
2013-04-09 09:54:40 +0700

    Add missing import in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='powerlinex-segment-plenv',
-      packages=find_packages(),
-      namespace_packages=['powerlinex']
-)
+setup(
+        name='powerlinex-segment-plenv',
+        packages=find_packages(),
+        namespace_packages=['powerlinex']
+        )
